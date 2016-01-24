b4ecf291e99d2db3046c2d23d11a05b7894f264e
codyss <cody.schwarz@gmail.com>
2016-01-23 21:33:23 -0500

    template rendering set up and working on index

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 
 // var fb = require('./customfield');
-var app = require('express')();
+var express = require('express');
+var app = express();
 var port = process.env.PORT || 8080;
 var routes = require('./routes');
 var swig = require('swig');
 var path = require('path');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
 
 
 // templating boilerplate setup
@@ -13,7 +16,17 @@ app.set('view engine', 'html'); // what file extension do our templates have
 app.engine('html', swig.renderFile); // how to render html templates
 swig.setDefaults({ cache: false });
 
+app.use(bodyParser.urlencoded({ extended: true })); // for HTML form submits
+
+
 app.use('/', routes);
+
+// logging middleware
+app.use(morgan('dev'));
 
 app.listen(port);
 console.log('Magic happening on: ', port);
+
+
+// use express static middleware.
+app.use(express.static(path.join(__dirname, '/public')));
--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,10 @@ var fieldbook = require('node-fieldbook');
 router.use(bodyParser.urlencoded({extended: false}));
 router.use(bodyParser.json());
 
-
 router.use(function (req, res, next) {
     console.log(req.path, req.method);
     next();
-})
+});
 
 var sheet = 'nyccourses';
 
@@ -19,19 +18,25 @@ var book = new fieldbook({
             username: 'key-1',
             password: 'w8vy4ZJcVEtBvzUGkm3V',
             book: '56a3cf62986e730300c844f4'
-            })
+            });
 
 var course;
 
 router.get('/', function (req, res, next) {
   book.getSheet(sheet)
-  .then(function (data) {
-    var cleaned = data.map(function (item) {
-        return item.course;
+  // .then(function (data) {
+  //   var cleaned = data.map(function (item) {
+  //       return item.course;
+  //   });
+  //   return cleaned;
+  // })
+  .then(function(data) {
+    var cleanedCourses = data.map(function (item) {
+      return {course: item.course, city: item.city, state: item.state};
     });
-    return cleaned;
+    res.render('index', {courses: cleanedCourses});
   })
-  .then(function (data) {res.status(200).json(data);})
+  // .then(function (data) {res.status(200).json(data);})
   .catch(function(err) {console.log(err);});
 });
 
@@ -41,4 +46,13 @@ router.get('/:course', function (req, res, next) {
   .then(function(data) {{res.status(200).json(data);}})
   .catch(function(err) {console.log(err);});
 });
+
+
+// router.get('/tweets/:id', function(req, res, next){
+//   var tweetsWithThatId = tweetBank.find({ id: Number(req.params.id) });
+//   res.render('index', {
+//     title: 'Twitter.js',
+//     tweets: tweetsWithThatId // an array of only one element ;-)
+//   });
+// });
 
