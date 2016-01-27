8e00f9d0e11de6d585d5ac9496dc03df3661c33f
codyss <cody.schwarz@gmail.com>
2016-01-26 19:37:05 -0500

    Schemas for course, player, score

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-mongoose.connect('mongodb://localhost/teetimes');
+mongoose.connect('mongodb://localhost/golfdemo');
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'mongodb connection error: '));
@@ -19,8 +19,30 @@ var courseSchema = new Schema({
   website: String
 });
 
+var playerSchema = new Schema({
+  name: {type: String, required: true},
+  email: {type: String, require: true, unique: true},
+  handicap: Number,
+  age: Number
+});
+
+var scoreSchema = new Schema({
+  score: Number,
+  differential: Number,
+  course: {type: mongoose.Schema.Types.ObjectId, ref: 'Course'},
+  player: {type: mongoose.Schema.Types.ObjectId, ref: 'Player'}
+});
 
 
+var Course = mongoose.model('Course', courseSchema);
+var Player = mongoose.model('Player', playerSchema);
+var Score = mongoose.model('Score', scoreSchema);
+
+module.exports = {
+  Course: Course,
+  Player: Player,
+  Score: Score
+};
 
 
 
